eef6d71bf09dc3681239018e15b9e3a4ef7ee3d6
ms1419 <ms1419@imperial.ac.uk>
2020-12-26 23:48:21 +0000

    Added signal_func arg to strategy_1 to allow the use of any indicator signal function

--- trading_bot.py
+++ trading_bot.py
@@ -36,30 +36,25 @@ class TradingBot:
 
         self.dispatcher = dispatcher
 
-    def strategy_1 (self):
+    def strategy_1 (self, signal_func):
         while(True):
             ohlc = self.dispatcher.get_ohlc_data(self.kraken)
             
             curr_candle = ohlc.iloc[-1]
-            crossing_candle = ohlc.iloc[-3] # Candle that crossed the SMA if signal is BUY or SELL
 
-            signal = sma_signal(ohlc[:-1])
+            signal = signal_func(ohlc[:-1])
 
             curr_ask = self.dispatcher.current_ask_price("ADAEUR")
             curr_bid = self.dispatcher.current_bid_price("ADAEUR")
 
-            self.dispatcher.buys.append(np.nan)
-            self.dispatcher.sells.append(np.nan)
-            if signal == Signal.BUY and isGreen(crossing_candle):
-                if curr_ask >= curr_candle['open']:
+            if signal == Signal.BUY:
+                if isGreen(curr_candle):
                     print("Buy signal")
                     self.dispatcher.buy("ADAEUR", int(self.dispatcher.balance / curr_ask), curr_ask)
-                    self.dispatcher.buys[-1] = curr_ask
-            elif signal == Signal.SELL and isRed(crossing_candle):
-                if curr_bid <= curr_candle['open']:
+            elif signal == Signal.SELL:
+                if isRed(curr_candle):
                     print("Sell signal")
                     self.dispatcher.sell("ADAEUR", curr_bid)
-                    self.dispatcher.sells[-1] = curr_bid
 
 if __name__ == "__main__":
     api = krakenex.API()
