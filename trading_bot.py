103ed37dda4d8831227b3f9d2f032e37a6b8f3ae
ms1419 <ms1419@imperial.ac.uk>
2020-12-27 16:16:03 +0000

    Added strategy_2

--- trading_bot.py
+++ trading_bot.py
@@ -49,12 +49,28 @@ class TradingBot:
 
             if signal == Signal.BUY:
                 if isGreen(curr_candle):
-                    print("Buy signal")
                     self.dispatcher.buy("ADAEUR", int(self.dispatcher.balance / curr_ask), curr_ask)
             elif signal == Signal.SELL:
                 if isRed(curr_candle):
-                    print("Sell signal")
                     self.dispatcher.sell("ADAEUR", curr_bid)
+    
+    def strategy_2 (self):
+        while(True):
+            ohlc = self.dispatcher.get_ohlc_data(self.kraken)
+            
+            curr_candle = ohlc.iloc[-1]
+
+            signal = ema_crosses_higher_lower_sma_signal(ohlc[:-1])
+            stoch_signal = stochastic_oscillator_signal(ohlc[:-1], 14, 3)
+
+            curr_ask = self.dispatcher.current_ask_price("ADAEUR")
+            curr_bid = self.dispatcher.current_bid_price("ADAEUR")
+
+            if signal == Signal.BUY and stoch_signal != Signal.SELL:
+                self.dispatcher.buy("ADAEUR", int(self.dispatcher.balance / curr_ask), curr_ask)
+            elif signal == Signal.SELL and stoch_signal != Signal.BUY:
+                self.dispatcher.sell("ADAEUR", curr_bid)
+
 
 if __name__ == "__main__":
     api = krakenex.API()
@@ -64,4 +80,22 @@ if __name__ == "__main__":
 
     ohlc, _ = kraken.get_ohlc_data("ADAEUR", interval=5, ascending=True)
 
-    display_graph(ohlc, 400)
+    buy, sell = chart_signals(ohlc, stochastic_oscillator_signal, stochastic_oscillator)
+
+    import matplotlib
+    import mplfinance as mpf
+    import pandas as pd
+    
+    stochastic_line = []
+    for i in range(1,ohlc['open'].size+1):
+        stochastic_line.append(stochastic_oscillator(ohlc.iloc[:i], 14))
+    stochastic_line = pd.Series(stochastic_line)
+
+    r = 400
+    display_graph(ohlc, r,
+        mpf.make_addplot(buy[-r:], type='scatter', markersize=100, marker='*', color='g', panel=1, secondary_y=False),
+        mpf.make_addplot(sell[-r:], type='scatter', markersize=100, marker='*', color='r', panel=1, secondary_y=False),
+        mpf.make_addplot(stochastic_line[-r:], panel=1, secondary_y=False),
+        mpf.make_addplot(stochastic_line.rolling(3).mean()[-r:], panel=1, secondary_y=False, marker='-'),
+        mpf.make_addplot(pd.Series(80, index=range(r)), panel=1, secondary_y=False, color='black'),
+        mpf.make_addplot(pd.Series(20, index=range(r)), panel=1, secondary_y=False, color='black'))
