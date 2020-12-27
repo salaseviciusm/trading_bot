86711c70d8b3a2e22ae136f39eb2ec9b9d60ff1f
ms1419 <ms1419@imperial.ac.uk>
2020-12-27 16:15:38 +0000

    Added stochastic oscillator signal and value functions

--- indicators.py
+++ indicators.py
@@ -1,4 +1,5 @@
 from candlestick import isGreen, isRed
+import pandas as pd
 
 class Signal:
     BUY = 0
@@ -45,12 +46,59 @@ def higher_lower_sma_signal(ohlc):
     return Signal.HOLD
 
 def ema_crosses_higher_lower_sma_signal(ohlc):
-    high_sma = SMA(ohlc['high'], 40)
-    low_sma = SMA(ohlc['low'], 40)
+    high_sma = SMA(ohlc['high'], 30)
+    low_sma = SMA(ohlc['low'], 30)
 
     prev_candle = ohlc.iloc[-2]
-    if EMA(ohlc['low'], 15) > high_sma:
+    if EMA(ohlc['high'], 15) > high_sma:
         return Signal.BUY
     if EMA(ohlc['low'], 15) < low_sma:
         return Signal.SELL
+    return Signal.HOLD
+
+def stochastic_oscillator(ohlc, period=14):
+    """
+    Returns the value of K% for the latest candle in ohlc based on the given period
+    """
+    p = min(ohlc['low'].size, period)
+
+    # Lowest low in the period
+    ll = ohlc[-p:].min()['low']
+    
+    # Highest high in the period
+    hh = ohlc[-p:].max()['high']
+
+    k = 100*(ohlc.iloc[-1]['close']-ll)/(hh-ll)
+
+    return k
+
+def close(ohlc):
+    return ohlc.iloc[-1]['close']
+
+def stochastic_oscillator_signal(ohlc, kperiod=14, dperiod=3):
+    """
+    Returns the signal returned by a stochastic oscillator indicator with kperiod on the calculation and
+    dperiod SMA of the K value.
+    Returns BUY when there are oversold conditions and the K line crosses above the D line
+    """
+
+    if ohlc['close'].size < max(kperiod, dperiod):
+        return Signal.HOLD
+
+    kvals = []
+    for i in range (1, dperiod):
+        kvals.append(stochastic_oscillator(ohlc[:-dperiod+i], kperiod))
+    kvals.append(stochastic_oscillator(ohlc, kperiod))
+
+    k = pd.Series(kvals)
+    d = SMA(k, dperiod)
+
+    if k.iloc[-1] < 20 and d < 20:
+        if k.iloc[-1] > d :
+            return Signal.BUY
+
+    if k.iloc[-1] > 80 and d > 80:
+        if k.iloc[-1] < d:
+            return Signal.SELL
+
     return Signal.HOLD
